07f022b341735e134c23c4f8fcd2b9e332769f58
ehayik <ehayik@tutanota.com>
2023-11-11 22:07:28 +0200

    initial commit

---
+++ src/main/java/org/github/ehayik/playground/SpringDataJpaKataApplication.java
@@ -0,0 +1,13 @@
+package org.github.ehayik.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaKataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaKataApplication.class, args);
+    }
+
+}
