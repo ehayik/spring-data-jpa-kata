0dbab5d032b0834a1814c7f7ddd579b07f8818e7
ehayik <ehayik@tutanota.com>
2023-11-11 22:27:35 +0200

    Setup  database connection and SQL tracing

--- src/main/java/org/github/ehayik/playground/Application.java
+++ src/main/java/org/github/ehayik/playground/Application.java
@@ -1,8 +1,15 @@
 package org.github.ehayik.playground;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
+import org.springframework.context.annotation.Bean;
 
+import java.time.LocalDate;
+
+@Slf4j
 @SpringBootApplication
 public class Application {
 
@@ -10,4 +17,25 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    CommandLineRunner logConnectionDetails(JdbcConnectionDetails jdbc) {
+        return args -> {
+            var details = STR. """
+                    class: \{ jdbc.getClass().getSimpleName() }
+                    JDBC URL: \{ jdbc.getJdbcUrl() }
+                    Username: \{ jdbc.getUsername() }
+                    Password: \{ jdbc.getPassword() }
+                    """ ;
+            log.info("Database connection details: {}", details);
+        };
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(EmployeeRepository employeeRepository) {
+        return args -> {
+            var afterDate = LocalDate.now().minusYears(30);
+            var count = employeeRepository.countAllByHireDateAfter(afterDate);
+            log.info("Found {} employees hired after {}", count, afterDate);
+        };
+    }
 }
